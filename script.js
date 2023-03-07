bc210488b298a02b8fa7a94ae22494752baa3e93
MeL4 <miskakurcikova82@gmail.com>
2023-03-07 20:32:37 +0100

    function that plays a round of rock paper scissors

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 
-const Options = ["rock", "paper", "scissors"];
+const ROCK = "rock"
+const PAPER = "paper"
+const SCISSORS = "scissors"
+
+const Options = [ROCK, PAPER, SCISSORS];
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3);
     return Options[randomChoice];
 }
 
-console.log(getComputerChoice());
+
+function playARound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "a draw";
+    }
+    else if ((playerSelection == ROCK && computerSelection == PAPER) || (playerSelection == PAPER && computerSelection == SCISSORS) ||
+    (playerSelection == SCISSORS && computerSelection == ROCK)) {
+        return "you loose";
+    }
+    else {
+        return "you win";
+    }
+}
